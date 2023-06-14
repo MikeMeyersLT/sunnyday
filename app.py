a38bf211ef18d8a49c9c2185723d71a0a85c0096
EntornoJT <vkurylo417@gmail.com>
2023-06-14 13:13:19 +0100

    gpt4

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationSummaryBufferMemory
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from pydantic import BaseModel
 from langchain.callbacks import AsyncIteratorCallbackHandler
 from typing import AsyncIterable, Awaitable
@@ -15,7 +16,8 @@ app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=[
                    "*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-memory=ConversationSummaryBufferMemory(llm=OpenAI())
+memory = ConversationSummaryBufferMemory(llm=OpenAI())
+
 
 class Message(BaseModel):
     message: str
@@ -26,7 +28,8 @@ async def send_message(message: str) -> AsyncIterable[str]:
     callback = AsyncIteratorCallbackHandler()
 
     conversation_chain = ConversationChain(
-        llm=OpenAI(streaming=True, callbacks=[callback]),
+        llm=ChatOpenAI(streaming=True, callbacks=[
+                       callback], model="gpt-4", max_tokens=8000),
         memory=memory,
     )
 
