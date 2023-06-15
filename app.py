e2be701b6423dc6e86da64b9ca61d3e962625c21
MikeMeyersLT <136553809+MikeMeyersLT@users.noreply.github.com>
2023-06-15 13:27:49 +0300

    Update app.py
    testing with gpt3.5 model, but not sure if it is working here

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ async def send_message(message: str) -> AsyncIterable[str]:
 
     conversation_chain = ConversationChain(
         llm=ChatOpenAI(streaming=True, callbacks=[
-                       callback], model="gpt-55", max_tokens=8000),
+                       callback], model="gpt-3.5-turbo", max_tokens=8000),
         memory=memory,
     )
 
